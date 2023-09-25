81aa52c586bb696162e96de904ac60f726c7f189
connor <connorriley@outlook.com>
2023-09-25 17:32:09 -0400

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bruh
+          5:32
         </p>
         <a
           className="App-link"
